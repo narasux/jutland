9722003e041276f00c92bdb61bc4dcb6e953e29c
schnee <suzh9@mail2.sysu.edu.cn>
2024-05-16 22:18:15 +0800

    minor: update ai default instruction

--- pkg/mission/controller/computer/computer.go
+++ pkg/mission/controller/computer/computer.go
@@ -1,10 +1,15 @@
 package computer
 
 import (
+	"fmt"
+	obj "github.com/narasux/jutland/pkg/mission/object"
+	"math/rand"
+
 	"github.com/narasux/jutland/pkg/mission/controller"
 	"github.com/narasux/jutland/pkg/mission/faction"
 	instr "github.com/narasux/jutland/pkg/mission/instruction"
 	"github.com/narasux/jutland/pkg/mission/state"
+	"github.com/narasux/jutland/pkg/utils/geometry"
 )
 
 // ComputerDecisionHandler 电脑决策处理器
@@ -24,21 +29,29 @@ var _ controller.InputHandler = (*ComputerDecisionHandler)(nil)
 func (h *ComputerDecisionHandler) Handle(misState *state.MissionState) map[string]instr.Instruction {
 	instructions := map[string]instr.Instruction{}
 
-	// TODO 移除 测试 AI 指令：如果范围 20 内有战舰，则移动到它附近
-	//for _, ship := range misState.Ships {
-	//	if ship.BelongPlayer != h.player {
-	//		continue
-	//	}
-	//	for _, ship2 := range misState.Ships {
-	//		if ship2.BelongPlayer == h.player {
-	//			continue
-	//		}
-	//		if geometry.CalcDistance(ship.CurPos.RX, ship.CurPos.RY, ship2.CurPos.RX, ship2.CurPos.RY) < 20 {
-	//			instructions[fmt.Sprintf("%s-%s", ship.Uid, instr.NameShipMove)] = instr.NewShipMove(ship.Uid, ship2.CurPos)
-	//			break
-	//		}
-	//	}
-	//}
+	// TODO 移除 测试 AI 指令：如果本身静止，范围 20 内有敌方战舰，且正在移动，则随机移动
+	for _, ship := range misState.Ships {
+		if ship.BelongPlayer != h.player {
+			continue
+		}
+		for _, enemyShip := range misState.Ships {
+			if enemyShip.BelongPlayer == h.player {
+				continue
+			}
+			distance := geometry.CalcDistance(ship.CurPos.RX, ship.CurPos.RY, enemyShip.CurPos.RX, enemyShip.CurPos.RY)
+			if distance < 20 && ship.CurSpeed == 0 && enemyShip.CurSpeed != 0 {
+				x, y := rand.Intn(9)-4, rand.Intn(9)-4
+				// 通过 ShipMove 指令实现散开行为
+				instructions[fmt.Sprintf("%s-%s", ship.Uid, instr.NameShipMove)] = instr.NewShipMove(
+					ship.Uid, obj.NewMapPos(
+						misState.Ships[ship.Uid].CurPos.MX+x,
+						misState.Ships[ship.Uid].CurPos.MY+y,
+					),
+				)
+				break
+			}
+		}
+	}
 
 	return instructions
 }
