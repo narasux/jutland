668f42bc52b1962d5cce850c11f4c68a242f5599
schnee <suzh9@mail2.sysu.edu.cn>
2024-10-02 21:18:29 +0800

    feat: add torpedo boat MTB-77

--- pkg/resources/images/bullet/bullet.go
+++ pkg/resources/images/bullet/bullet.go
@@ -8,40 +8,42 @@ import (
 )
 
 var (
-	GB1024 = ebutil.NewImageWithColor(6, 14, colorx.DarkRed)
-	GB460  = ebutil.NewImageWithColor(4, 9, colorx.Gold)
-	GB406  = ebutil.NewImageWithColor(4, 8, colorx.Silver)
-	GB381  = ebutil.NewImageWithColor(4, 7, colorx.Gold)
-	GB356  = ebutil.NewImageWithColor(4, 6, colorx.Gold)
-	GB343  = ebutil.NewImageWithColor(3, 6, colorx.White)
-	GB305  = ebutil.NewImageWithColor(3, 5, colorx.Silver)
-	GB283  = ebutil.NewImageWithColor(3, 5, colorx.Gray)
-	GB203  = ebutil.NewImageWithColor(3, 4, colorx.White)
-	GB180  = ebutil.NewImageWithColor(2, 4, colorx.Gray)
-	GB155  = ebutil.NewImageWithColor(2, 4, colorx.Gold)
-	GB152  = ebutil.NewImageWithColor(2, 4, colorx.White)
-	GB150  = ebutil.NewImageWithColor(2, 4, colorx.Gray)
-	GB140  = ebutil.NewImageWithColor(2, 3, colorx.Silver)
-	GB133  = ebutil.NewImageWithColor(2, 3, colorx.DarkBlue)
-	GB130  = ebutil.NewImageWithColor(2, 3, colorx.White)
-	GB127  = ebutil.NewImageWithColor(2, 3, colorx.Silver)
-	GB120  = ebutil.NewImageWithColor(2, 3, colorx.DarkBlue)
-	GB114  = ebutil.NewImageWithColor(2, 2, colorx.Silver)
-	GB105  = ebutil.NewImageWithColor(2, 2, colorx.Gray)
-	GB102  = ebutil.NewImageWithColor(2, 2, colorx.Silver)
-	GB100  = ebutil.NewImageWithColor(2, 2, colorx.White)
-	GB88   = ebutil.NewImageWithColor(2, 2, colorx.Gold)
-	GB40   = ebutil.NewImageWithColor(1, 1, colorx.Gold)
-	GB37   = ebutil.NewImageWithColor(1, 1, colorx.DarkBlue)
-	GB20   = ebutil.NewImageWithColor(1, 1, colorx.White)
-	GB13   = ebutil.NewImageWithColor(1, 1, colorx.Gray)
+	gb1024 = ebutil.NewImageWithColor(6, 14, colorx.DarkRed)
+	gb460  = ebutil.NewImageWithColor(4, 9, colorx.Gold)
+	gb406  = ebutil.NewImageWithColor(4, 8, colorx.Silver)
+	gb381  = ebutil.NewImageWithColor(4, 7, colorx.Gold)
+	gb356  = ebutil.NewImageWithColor(4, 6, colorx.Gold)
+	gb343  = ebutil.NewImageWithColor(3, 6, colorx.White)
+	gb305  = ebutil.NewImageWithColor(3, 5, colorx.Silver)
+	gb283  = ebutil.NewImageWithColor(3, 5, colorx.Gray)
+	gb203  = ebutil.NewImageWithColor(3, 4, colorx.White)
+	gb180  = ebutil.NewImageWithColor(2, 4, colorx.Gray)
+	gb155  = ebutil.NewImageWithColor(2, 4, colorx.Gold)
+	gb152  = ebutil.NewImageWithColor(2, 4, colorx.White)
+	gb150  = ebutil.NewImageWithColor(2, 4, colorx.Gray)
+	gb140  = ebutil.NewImageWithColor(2, 3, colorx.Silver)
+	gb133  = ebutil.NewImageWithColor(2, 3, colorx.DarkBlue)
+	gb130  = ebutil.NewImageWithColor(2, 3, colorx.White)
+	gb127  = ebutil.NewImageWithColor(2, 3, colorx.Silver)
+	gb120  = ebutil.NewImageWithColor(2, 3, colorx.DarkBlue)
+	gb114  = ebutil.NewImageWithColor(2, 2, colorx.Silver)
+	gb105  = ebutil.NewImageWithColor(2, 2, colorx.Gray)
+	gb102  = ebutil.NewImageWithColor(2, 2, colorx.Silver)
+	gb100  = ebutil.NewImageWithColor(2, 2, colorx.White)
+	gb88   = ebutil.NewImageWithColor(2, 2, colorx.Gold)
+	gb40   = ebutil.NewImageWithColor(1, 1, colorx.Gold)
+	gb37   = ebutil.NewImageWithColor(1, 1, colorx.DarkBlue)
+	gb20   = ebutil.NewImageWithColor(1, 1, colorx.White)
+	gb13   = ebutil.NewImageWithColor(1, 1, colorx.Gray)
+	// gb12 其实就是 12.7mm，懒得改成 float，就这样吧 :D
+	gb12 = ebutil.NewImageWithColor(1, 1, colorx.White)
 )
 
 var (
-	TB450 = ebutil.NewImageWithColor(3, 16, colorx.Silver)
-	TB533 = ebutil.NewImageWithColor(3, 20, colorx.DarkSilver)
-	TB610 = ebutil.NewImageWithColor(4, 24, colorx.Silver)
-	TB622 = ebutil.NewImageWithColor(4, 25, colorx.Gray)
+	tb450 = ebutil.NewImageWithColor(3, 16, colorx.Silver)
+	tb533 = ebutil.NewImageWithColor(3, 20, colorx.DarkSilver)
+	tb610 = ebutil.NewImageWithColor(4, 24, colorx.Silver)
+	tb622 = ebutil.NewImageWithColor(4, 25, colorx.Gray)
 )
 
 var NotFount = ebutil.NewImageWithColor(10, 20, colorx.Red)
@@ -50,59 +52,61 @@ var NotFount = ebutil.NewImageWithColor(10, 20, colorx.Red)
 func GetShell(diameter int) *ebiten.Image {
 	switch diameter {
 	case 1024:
-		return GB1024
+		return gb1024
 	case 460:
-		return GB460
+		return gb460
 	case 406:
-		return GB406
+		return gb406
 	case 381:
-		return GB381
+		return gb381
 	case 356:
-		return GB356
+		return gb356
 	case 343:
-		return GB343
+		return gb343
 	case 305:
-		return GB305
+		return gb305
 	case 283:
-		return GB283
+		return gb283
 	case 203:
-		return GB203
+		return gb203
 	case 180:
-		return GB180
+		return gb180
 	case 155:
-		return GB155
+		return gb155
 	case 152:
-		return GB152
+		return gb152
 	case 150:
-		return GB150
+		return gb150
 	case 140:
-		return GB140
+		return gb140
 	case 133:
-		return GB133
+		return gb133
 	case 130:
-		return GB130
+		return gb130
 	case 127:
-		return GB127
+		return gb127
 	case 120:
-		return GB120
+		return gb120
 	case 114:
-		return GB114
+		return gb114
 	case 105:
-		return GB105
+		return gb105
 	case 102:
-		return GB102
+		return gb102
 	case 100:
-		return GB100
+		return gb100
 	case 88:
-		return GB88
+		return gb88
 	case 40:
-		return GB40
+		return gb40
 	case 37:
-		return GB37
+		return gb37
 	case 20:
-		return GB20
+		return gb20
 	case 13:
-		return GB13
+		return gb13
+	case 12:
+		return gb12
 	}
 	// 找不到就暴露出来
 	return NotFount
@@ -112,13 +116,13 @@ func GetShell(diameter int) *ebiten.Image {
 func GetTorpedo(diameter int) *ebiten.Image {
 	switch diameter {
 	case 450:
-		return TB450
+		return tb450
 	case 533:
-		return TB533
+		return tb533
 	case 610:
-		return TB610
+		return tb610
 	case 622:
-		return TB622
+		return tb622
 	}
 	// 找不到就暴露出来
 	return NotFount
